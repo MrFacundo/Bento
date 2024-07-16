7b62b6f22ea103cd0b422f134fd61138717649dc
Facundo Troitero <ftroitero@gmail.com>
2024-07-16 16:03:03 +0100

    f

--- config.js
+++ config.js
@@ -71,9 +71,9 @@ const CONFIG = {
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.google.com/',
 		},
 		{
 			id: '5',
